c1dc0a9e62782b985670284eac8f8626fef1b594
Atakan_Yigit_Cengeloglu <atakanyigit85@gmail.com>
2024-05-23 17:14:49 +0300

    - Admin Edit Added

--- admin_panel/edit_admin.php
+++ admin_panel/edit_admin.php
@@ -1,4 +1,28 @@
 <?php include("config.php");?>
+
+<?php 
+    $url = $_SERVER['REQUEST_URI'];
+    $parsedUrl = parse_url($url);
+
+    if (isset($parsedUrl['query'])) {
+        parse_str($parsedUrl['query'], $queryParams);
+        if (isset($queryParams['id'])) {
+            $admin_id = $queryParams['id'];
+        }else{
+            echo "ID not found in the URL";
+        }
+    }else{
+        echo "No query string found in the URL";
+    }
+
+    $admin = query_parser("SELECT * FROM admin_table WHERE admin_id = $admin_id")[0];
+    
+    $admin_name = $admin['admin_name'];
+    $admin_surname = $admin['admin_surname'];
+    $admin_username = $admin['admin_username'];
+    $admin_pass = $admin['admin_pass'];
+?>
+
 <!DOCTYPE html>
 <html class="no-js">
     <head>
@@ -19,26 +43,25 @@
     <?php include('top_bar.php'); ?>
     <?php include('left_sidebar.php'); ?>
     <body>
-        
         <div id="login-container">
             <div id="addProduct">
-                <h3>Add New Admin</h3>
-                <form action="add_admin_service.php" method="post" enctype="multipart/form-data">
+                <h1>Edit Admin: <?php echo $admin_name ?></h1>
+                <form action="edit_admin_service.php/?id=<?php echo $admin_id ?>" method="post" enctype="multipart/form-data">
                     <div class="mb-3">
                         <label for="exampleInputName" class="form-label">Admin Name</label>
-                        <input type="text" name="name" class="form-control" id="exampleInputName">
+                        <input type="text" name="name" class="form-control" id="exampleInputType" value = "<?php echo $admin_name ?>">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputType" class="form-label">Admin Surname</label>
-                        <input type="text" name="surname"  class="form-control" id="exampleInputType">
+                        <input type="text" name="surname"  class="form-control" id="exampleInputDescription" value = "<?php echo $admin_surname ?>">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputDescription" class="form-label">User Name</label>
-                        <input type="text" name="userName" class="form-control" id="exampleInputDescription">
+                        <input type="text" name="userName" class="form-control" id="exampleInputQuantity" value = "<?php echo $admin_username?>">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputQuantity" class="form-label">Password</label>
-                        <input type="text" name="password" class="form-control" id="exampleInputQuantity">
+                        <input type="text" name="password" class="form-control" id="exampleInputPrice" value = "<?php echo $admin_pass?>">
                     </div>
                     <button type="submit" name="submit" class="btn btn-primary">Submit</button>
                 </form>
---
+++ admin_panel/edit_admin_service.php
@@ -0,0 +1,39 @@
+<?php
+    $url = $_SERVER['REQUEST_URI'];
+    $parsedUrl = parse_url($url);
+
+    if (isset($parsedUrl['query'])) {
+        parse_str($parsedUrl['query'], $queryParams);
+        if (isset($queryParams['id'])) {
+            $admin_id = $queryParams['id'];
+        }else{
+            echo "ID not found in the URL";
+        }
+    }else{
+        echo "No query string found in the URL";
+    }
+
+    $result=null;
+    if (isset($_POST['submit'])){
+        $servername = "localhost";
+        $username   = "root";
+        $password   = "";
+        $dbname     = "store_db";
+        $conn       = new mysqli($servername, $username, $password, $dbname);
+
+        $name        = $_POST['name'];
+        $surname     = $_POST['surname'];
+        $user_name   = $_POST['userName'];
+        $password    = $_POST['password'];
+        //Update Admin By Id
+        $insertSql = "UPDATE admin_table SET admin_name = '$name', admin_surname = '$surname', admin_username = '$user_name', admin_pass = '$password' WHERE admin_id = $admin_id";
+
+        if ($conn -> query ($insertSql)){
+            $result="<h2>*******Data insert success*******</h2>";
+            //navigate to dashboard.php
+            header("Location: /Basic-E-Commerce-Application/admin_panel/dashboard.php");
+        }else{
+            die($conn -> error);
+        }
+    } 
+?>
