bd3342df4710424d86b902f6f6a3d644132388e9
Atakan_Yigit_Cengeloglu <atakanyigit85@gmail.com>
2024-05-29 17:39:58 +0300

    - Categories Page Fix

--- user_front/category.php
+++ user_front/category.php
@@ -67,26 +67,57 @@
     </head>
     <body>
         <?php include("navbar.php"); ?>
+        <script>
+            const handle_add_to_cart_click = (e, id, count) =>{
+                e.preventDefault();
+                fetch("add_to_cart_service.php", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded"
+                    },
+                    body: "id=" + id + "&count=" + count
+                })
+                    .then(response => {
+                        if (response.ok) {
+                            console.log(response);
+                            location.reload();
+                        } else {
+                            throw new Error("Failed to add product to cart");
+                        }
+                    })
+                    .catch(error => {
+                        console.error(error);
+                        alert("Failed to add product to cart")
+                    });
+                }
+        </script>
+
         <main class = "ps-5 pt-3">
             <h1>Welcome to Yigit's Store</h1>
             <p class = "mb-4">Yigit's Store is a leading online store that offers a wide range of products at competitive prices. Our goal is to provide our customers with the best shopping experience possible. We offer a wide selection of products, including electronics, clothing, home goods, and more. Whether you're looking for the latest tech gadgets or stylish fashion accessories, you'll find it all at Yigit's Store. Shop with us today and experience the difference!</p>
-            <div class = "d-flex justify-content-between gap-3">
-            <?php
-                foreach (query_parser("SELECT p.* FROM products AS p INNER JOIN categories AS c ON p.category_id = c.id WHERE c.id = $category_id") as $product) {
-                ?>
-                    <form class = "card rounded" style = "width: 20%;" action = "direct_order.php?id=<?php echo $product["id"]; ?>" method = "POST">
-                        <img src="data:image/png;base64,<?php echo base64_encode($product["image"]); ?>" class="card-img-top" alt="<?php echo $product["name"]; ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $product["name"]; ?></h5>
-                            <p class="card-text"><?php echo $product["description"]; ?></p>
-                            <input type = "submit" class = "btn btn-primary" value = "Buy Now"/>
-                            <a href="#" class="btn btn-secondary">Add to Cart</a>
-                            <a href="#" class="btn btn-secondary">Details</a>
-                        </div>
-                    </form>
-                <?php
-                }
-                ?>            
+            <div class = "d-flex flex-row justify-content-center gap-5 w-100">
+                <div class = "d-flex justify-content-start gap-5 flex-wrap mb-5" style = "width: 85%">
+                    <?php foreach (query_parser("SELECT p.* FROM products AS p INNER JOIN categories AS c ON p.category_id = c.id WHERE c.id = $category_id") as $product) { ?>
+                        <form class = "card rounded" style = "width: 20%;" action = "direct_order.php?id=<?php echo $product["id"]; ?>" method = "POST">
+                            <a href = "product_details.php?id=<?php echo $product["id"]; ?>" class = "card-img-top justify-content-center align-items-start d-flex overflow-hidden" style = "height: 200px;">
+                                <img src = "data:image/png;base64, <?php echo base64_encode($product["image"]); ?>" style = "max-width: 100%" alt = "<?php echo $product["name"]; ?>">
+                            </a>
+                            <div class = "card-body">
+                                <h5 class = "card-title"><?php echo $product["name"]; ?></h5>
+                                <p class = "card-text"><?php echo $product["description"]; ?></p>
+                                <p class = "card-text"><?php echo $product["price"]; ?>$</p>
+                                <div class = "d-flex flex-row gap-3">
+                                    <input type="number" name="count_to_buy_for_<?php echo $product["id"]?>" class="form-control w-50" id="exampleInputName" value="1" min = "1" max = "<?php echo $product["quantity"]; ?>">
+                                    <button class="btn btn-secondary w-50" style = "white-space:nowrap" type="button" onclick = "handle_add_to_cart_click(event, <?php echo $product['id']; ?>, document.querySelector('input[name=count_to_buy_for_<?php echo $product['id']; ?>').value)">Add to Cart</button>
+                                </div>
+                                <div class = "d-flex flex-row gap-3 mt-2 gap-3 align-items-center w-100">
+                                    <input type = "submit" class = "btn btn-primary w-50" value = "Buy Now"/>
+                                    <a class = "btn btn-secondary w-50" href = "product_details.php?id=<?php echo $product["id"]; ?>">Details</a>
+                                </div>
+                            </div>
+                        </form>
+                    <?php } ?>
+                </div>
             </div>
         </main>
         <script src = "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity = "sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin = "anonymous"></script>
